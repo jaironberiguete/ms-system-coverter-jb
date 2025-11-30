907d081f08a6a3b4319d40ec884b79be59f17c76
Jairon <jaironberiguete@gmail.com>
2025-11-29 19:22:24 -0500

    Add echo statements for Docker image operations

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,9 +2,11 @@
 set -e
 
 # Pull the Docker image from Docker Hub
+
 echo "Pull the Docker image from Docker Hub"
 docker pull jaironberiguete/auth
 
 # Run the Docker image as a container
+
 echo "Run the Docker image as a container"
 docker run -d -p 5000:8000 jaironberiguete/auth 
